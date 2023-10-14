a7508abe4e787ec9c5dd921a7fe1f12754899cd4
Otrackk <matteomontenero15@gmail.com>
2023-10-14 13:04:00 +0200

    final-commit

--- script/script.js
+++ script/script.js
@@ -5,9 +5,12 @@ var startButton = document.getElementById("#start-button");
 var container = document.getElementById("main-cont");
 var scoreDisplay = document.getElementById("score");
 var yourScore = document.getElementById("your-score");
-yourScore.style.display = "none";
+var yourRecord = document.getElementById("your-record");
 var score = 1;
 var dead = false;
+var highScore = [0];
+
+yourScore.style.display = "none";
 
 block.addEventListener("animationiteration", scoring);
 
@@ -21,6 +24,8 @@ function startAnimation() {
         scoreDisplay.innerHTML = "Score : 0";
     }
 
+    character.style.backgroundImage = "url(./assets/character-running.gif)";
+    block.style.backgroundImage = "url(./assets/enemy-running.gif)";
     start.innerHTML = "Jump!";
     block.classList.add("slide");
     dead = false;
@@ -32,6 +37,10 @@ function jump() {
         character.classList.add("jump");
     }
 
+    else if (character.classList == "jump") {
+        return;
+    }
+
     setTimeout(function () {
         character.classList.remove("jump");
     }, 500)
@@ -40,16 +49,37 @@ function jump() {
 
 function collision() {
     var a = setInterval(function () {
-        var topC = parseInt(window.getComputedStyle(character, null).getPropertyValue("top"));
-        var leftB = parseInt(window.getComputedStyle(block, null).getPropertyValue("left"));
 
-        if (topC <= 300 && topC >= 200 && leftB <= 100) {
-            
+        var topC = parseInt(window.getComputedStyle(character, null).getPropertyValue("top"));
+        var leftE = parseInt(window.getComputedStyle(block, null).getPropertyValue("left"));
+
+        if (topC <= 320 && topC >= 300 && leftE <= 50) {
+
+            if (character.classList == 'jump') {
+                character.classList.remove("jump");
+            }
+
+            document.body.style.backgroundColor = "#8a0303";
+            character.style.width = "200px";
+            character.style.backgroundImage = "url(./assets/character-dies.gif)";
+            block.style.backgroundImage = "url(./assets/enemy-standing.gif)";
+
+            setTimeout(() => {
+                document.body.style.backgroundColor = "#434352";
+                character.style.width = "100px";
+                character.style.backgroundImage = "url(./assets/character-standing.gif)";
+            }, 900);
+
             block.classList.remove("slide");
             scoreDisplay.innerHTML = "Your score is : " + (score - 1);
             start.innerHTML = "You're dead :(";
+            let newScore = score;
+            highScore.push(newScore - 1);
             score = 1;
             dead = true;
+            var largest = Math.max.apply(null, highScore);
+            yourRecord.innerHTML = "Your Record : " + largest;
+
         }
 
     }, 10);
